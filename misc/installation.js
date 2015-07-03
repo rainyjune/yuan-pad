3bff14307bb64e1b49ec8db41c00adb7985ffc08
rainyjune <rainyjune@live.cn>
2015-07-03 16:10:14 +0800

    Fixes errors

--- misc/installation.js
+++ misc/installation.js
@@ -1,27 +1,33 @@
 /**
- * @version $Id$
+ * @fileoverview The script files used in the installation.
  *
  */
 window.onload = initAll;
 function initAll() {
-    var submitButton = document.getElementById('submitButton');
-    var agreeButton = document.getElementById('agree');
-    var theForm = document.forms[0];
-    var dbTypeEle = document.getElementById('dbtype');
+  var submitButton = document.getElementById('submitButton');
+  var agreeButton = document.getElementById('agree');
+  var theForm = document.forms[0];
+  var dbTypeEle = document.getElementById('dbtype');
+  if (submitButton) {
     submitButton.disabled = 'disabled';
+  }
+  if (agreeButton) {
     agreeButton.onclick = function() {
-        if (agreeButton.checked) {
-            submitButton.disabled = "";
-        } else {
-            submitButton.disabled = "disabled";
-        }
+      if (agreeButton.checked) {
+        submitButton.disabled = "";
+      } else {
+        submitButton.disabled = "disabled";
+      }
     };
+  }
+  if (theForm) {
     theForm.onsubmit = function() {
-        if (dbTypeEle.value == "") {
-            var dbTip = dbTypeEle.options[0].text;
-            alert(dbTip);
-            return false;
-        }
-        return true;
+      if (dbTypeEle.value == "") {
+        var dbTip = dbTypeEle.options[0].text;
+        alert(dbTip);
+        return false;
+      }
+      return true;
     }
+  }
 }
