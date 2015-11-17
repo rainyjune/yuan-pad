9fe77a484d152adbf00a1d54f557ad67217629df
rainyjune <rainyjune@live.cn>
2015-11-17 15:33:09 +0800

    Add style.css

--- themes/spa/templates/index.php
+++ themes/spa/templates/index.php
@@ -6,6 +6,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.14.2/react.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.14.2/react-dom.min.js"></script>
     <script src="https://cdn.rawgit.com/rainyjune/yuanjs/master/build/yuan.min.js"></script>
+    <link rel="stylesheet" href="./themes/spa/css/style.css" />
   </head>
   <body>
     <div id="content"></div>
