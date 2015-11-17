7c3d94cc4bc30adc0dfb117c0c124c9ce2551a24
rainyjune <rainyjune@live.cn>
2015-11-17 10:57:03 +0800

    Use yuan.min.js

--- themes/spa/templates/index.php
+++ themes/spa/templates/index.php
@@ -2,9 +2,10 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>guest book</title>
+    <title><?php echo t('WELCOME',array('{site_name}'=>ZFramework::app()->board_name));?></title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.14.2/react.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.14.2/react-dom.min.js"></script>
+    <script src="https://cdn.rawgit.com/rainyjune/yuanjs/master/build/yuan.min.js"></script>
   </head>
   <body>
     <div id="content"></div>
