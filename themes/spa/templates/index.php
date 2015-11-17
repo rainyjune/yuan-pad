a2f89b090c54e70fe2b69f1dd349f5c42c5da0e6
rainyjune <rainyjune@live.cn>
2015-11-17 09:27:57 +0800

    Add themes/spa files

---
+++ themes/spa/templates/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>guest book</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.14.2/react.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.14.2/react-dom.js"></script>
+  </head>
+  <body>
+    <div id="content"></div>
+    <script src='./themes/spa/build/index.js'></script>
+  </body>
+</html>
