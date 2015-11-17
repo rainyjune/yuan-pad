f876d8efcd0449a719921e260b4cf0669b484032
rainyjune <rainyjune@live.cn>
2015-11-17 09:41:03 +0800

    Update spa/src/index.js

--- themes/spa/build/index.js
+++ themes/spa/build/index.js
@@ -1,4 +1,41 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-//
+var CommentBox = React.createClass({
+  displayName: "CommentBox",
+
+  render: function () {
+    return React.createElement(
+      "div",
+      { className: "commentBox" },
+      React.createElement(CommentList, null),
+      React.createElement(CommentForm, null)
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  displayName: "CommentList",
+
+  render: function () {
+    return React.createElement(
+      "div",
+      { className: "commentList" },
+      "This is a comment list."
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  displayName: "CommentForm",
+
+  render: function () {
+    return React.createElement(
+      "div",
+      { className: "commentForm" },
+      "This is a comment form."
+    );
+  }
+});
+
+ReactDOM.render(React.createElement(CommentBox, null), document.getElementById('content'));
 
 },{}]},{},[1]);
--- themes/spa/src/index.js
+++ themes/spa/src/index.js
@@ -1,1 +1,35 @@
-//
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        This is a comment list.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        This is a comment form.
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
