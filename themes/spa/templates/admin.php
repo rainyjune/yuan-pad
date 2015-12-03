cf10a4574caa1c99bff8d9b74c73205f1e25643c
rainyjune <rainyjune@live.cn>
2015-12-03 16:53:36 +0800

    Add admin.html in the spa theme

---
+++ themes/spa/templates/admin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title><?= t('ACP_INDEX') ?></title>
+    <script src="./themes/spa/build/yuan.min.js"></script>
+    <link rel="stylesheet" href="./themes/spa/css/acp.css">
+  </head>
+  <body>
+    <div id="content"></div>
+    <script src="./themes/spa/build/acp.js"></script>
+  </body>
+</html>
