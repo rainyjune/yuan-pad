591b5052493e5a46a29c5ac9b65f9d4af838983f
rainyjune <rainyjune@live.cn>
2015-12-06 15:02:10 +0800

    Add updateUser.js

---
+++ themes/spa/src/updateUser.js
@@ -0,0 +1,119 @@
+var React = require('react');
+var Modal = require('react-modal');
+var dataProvider = require('./dataProvider.js');
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+var UserUpdate = React.createClass({
+  getInitialState: function() {
+    return {
+      userUpdateErrorMsg: '',
+      userUpdateModalIsOpen: false
+    };
+  },
+  openUserUpdateModal: function() {
+    this.setState({userUpdateModalIsOpen: true});
+  },
+  closeUserUpdateModal: function() {
+    this.setState({userUpdateModalIsOpen: false});
+  },
+  handleUserUpdate: function(userData) {
+    dataProvider.updateUser(userData, function(data) {
+        console.log('update user result:', data, userData);
+        if (data.error) {
+          if (this.isMounted()) {
+            this.setState({userUpdateErrorMsg: data.error_detail});
+          }
+        } else {
+          if (this.isMounted()) {
+            this.setState({userUpdateErrorMsg: '', userUpdateModalIsOpen: false});
+            this.props.onUserUpdated();
+          }
+        }
+      }.bind(this),
+      function(xhr, status, err) {
+        debugger;
+      }.bind(this));
+  },
+  render: function() {
+    return this.props.user.user ?
+            (
+              <div>
+                <UserUpdateButton 
+                  lang={this.props.lang}
+                  onShowUpdateModal={this.openUserUpdateModal}
+                />
+                <UserUpdateModal
+                  lang={this.props.lang}
+                  userUpdateModalIsOpen={this.state.userUpdateModalIsOpen}
+                  userUpdateErrorMsg={this.state.userUpdateErrorMsg}
+                  userDetailedData={this.props.userDetailedData}
+                  onRequestClose={this.closeUserUpdateModal}
+                  onUserUpdateSubmit={this.handleUserUpdate}
+                />
+              </div>
+            ) : null;
+  }
+});
+
+var UserUpdateButton = React.createClass({
+  render: function() {
+    return (
+      <a href="javascript:void(0);" onClick={this.props.onShowUpdateModal}>{this.props.lang.UPDATE}</a>
+    );
+  }
+});
+
+var UserUpdateModal = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var uid = this.refs.uid.value.trim();
+    var user = this.refs.user.value.trim();
+    var pwd = this.refs.pwd.value.trim();
+    var email = this.refs.email.value.trim();
+    if (!uid || !user || !pwd || !email) return;
+    this.props.onUserUpdateSubmit({ uid: uid, user: user, pwd: pwd, email: email}); 
+    return false;
+  },
+  render: function(){
+    return (
+      <Modal isOpen={this.props.userUpdateModalIsOpen} onRequestClose={this.props.onRequestClose} style={customStyles} >
+        <h2>Update profile</h2>
+        <p>{this.props.userUpdateErrorMsg}</p>
+        <button onClick={this.props.onRequestClose}>close</button>
+        <form onSubmit={this.handleSubmit} action="#" method="post">
+        <input type="hidden" ref="uid" value={this.props.userDetailedData.uid} />        
+          <dl>
+          <dt>{this.props.lang.USERNAME}</dt>
+          <dd><input type="text" readOnly="readonly" defaultValue={this.props.userDetailedData.username} ref="user" size="20"  />
+          </dd>
+          </dl>
+          <dl>
+          <dt>{this.props.lang.PASSWORD}</dt>
+          <dd><input type="password" defaultValue={this.props.userDetailedData.password} ref="pwd" size="20"  />
+          </dd>
+          </dl>
+          <dl>
+          <dt>{this.props.lang.EMAIL}</dt>
+          <dd><input type="text" defaultValue={this.props.userDetailedData.email} ref="email" size="20"  />
+          </dd>
+          </dl>
+          <dl>
+              <dt><input type="submit" value={this.props.lang.UPDATE} /></dt>
+          </dl>
+        </form>
+      </Modal>
+    );
+  }
+});
+
+module.exports = UserUpdate;
