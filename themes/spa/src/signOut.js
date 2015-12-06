069d0d2f36f389389c0e770e3b61e4546cb4241f
rainyjune <rainyjune@live.cn>
2015-12-06 15:40:29 +0800

    Update singOut.js

--- themes/spa/src/signOut.js
+++ themes/spa/src/signOut.js
@@ -1,9 +1,17 @@
 var React = require('react');
+var dataProvider = require('./dataProvider.js');
+
 var LogoutButton = React.createClass({
+  handleSignOut: function(e) {
+    e.preventDefault();
+    dataProvider.logout(function(){
+      this.props.onUserLogout();
+    }.bind(this));
+  },
   render: function() {
     return (!this.props.user.admin && !this.props.user.user) ?
            null :
-           (<a href='javascript:void(0);' onClick={this.props.onUserLogout}>{this.props.lang.LOGOUT}</a>);
+           (<a href='#' onClick={this.handleSignOut}>{this.props.lang.LOGOUT}</a>);
   }
 });
 
