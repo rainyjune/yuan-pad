60f6249d56925d32c9b7989c14a8289437470141
rainyjune <rainyjune@live.cn>
2015-12-11 09:59:34 +0800

    cleanup

--- api.php
+++ api.php
@@ -9,6 +9,5 @@ define('APPROOT', dirname(__FILE__));
 define('DEBUG_MODE', true);
 define('API_MODE', true);
 include APPROOT.'/includes/api_code.php';
-#define('DEBUG_MODE', false);
 require_once('./includes/preload.php');
 ZFramework::app()->run();
