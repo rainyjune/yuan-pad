e88732d257cb82c143946033f979828d19988f59
rainyjune <rainyjune@live.cn>
2015-12-13 07:46:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,15 @@
 /**
  * @author rainyjune <rainyjune@live.cn>
  * @link https://github.com/rainyjune/yuan-pad 
- * @copyright Copyright &copy; 2008-2011 rainyjune
- * @license GPL2
- * @version $Id$
+ * @copyright Copyright &copy; 2008-2015 rainyjune
+ * @license MIT
  */
 
 session_start();
 define('IN_MP',true);
 define('APPROOT', dirname(__FILE__));
 define('DEBUG_MODE', true);
-#define('DEBUG_MODE', false);
+define('API_MODE', true);
 include APPROOT.'/includes/api_code.php';
 require_once('./includes/preload.php');
 ZFramework::app()->run();
