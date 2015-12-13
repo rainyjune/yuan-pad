c8ac769aeb5547ac77398b995997d382ecd4d56d
rainyjune <rainyjune@live.cn>
2015-12-13 11:41:44 +0800

    update PostController.php

--- includes/controllers/PostController.php
+++ includes/controllers/PostController.php
@@ -11,115 +11,103 @@ class PostController extends BaseController{
      *
      * @return mixed
      */
-    public function actionCreate(){
-        global $API_CODE;
-        if(isset ($_POST['user'])){
-            //Validation
-            $new_data_error_msg='';
-            if ( !strlen(trim($_POST['user'])) || !strlen(trim($_POST['content']))) {
-                $new_data_error_msg=t('FILL_NOT_COMPLETE');
-            } elseif(strlen($_POST['content']>580)) {
-                $new_data_error_msg=t('WORDS_TOO_LONG');
-            } elseif (ZFramework::app()->valid_code_open==1 && gd_loaded() && !$this->_verifyCode->check($_POST['valid_code'])) {
-                $new_data_error_msg=t('CAPTCHA_WRONG');
-            }
-            if($new_data_error_msg){
-                if(defined('API_MODE')){
-                    $error_array=array('error_code'=>'400','error'=>$API_CODE['400'],'error_detail'=>$new_data_error_msg);
-                    die(json_encode($error_array));
-                }
-                if(!empty ($_POST['ajax'])) {
-                    die ($new_data_error_msg);
-                } else {
-                    show_message ($new_data_error_msg, true, 'index.php');
-                }
-            }
-            // Ready to send query to database
-            $user=  $this->_model->escape_string($_POST['user']);
-            if(!isset($_SESSION['admin']) && $_POST['user']==ZFramework::app()->admin ) {
-                $user='anonymous';
-            }
-            $userExists=  $this->_model->queryAll(sprintf(parse_tbprefix("SELECT * FROM <sysuser> WHERE username='%s'"),  $this->_model->escape_string($_POST['user'])));
-            if($userExists && (@$_SESSION['user']!=$_POST['user'])) {
-                $user='anonymous';
-            }
-            $content = $this->_model->escape_string($_POST['content']);
-            if(isset ($_SESSION['uid'])) {
-                $sql_insert= sprintf (parse_tbprefix("INSERT INTO <post> ( uid , content , post_time , ip ) VALUES ( %d , '%s' , %d , '%s' )"), $_SESSION['uid'],$content,  time (),  getIp ());
-            } else {
-                $sql_insert = sprintf (parse_tbprefix("INSERT INTO <post> ( uname , content , post_time , ip ) VALUES ( '%s' ,'%s' , %d , '%s')"), $user,$content,  time (),  getIp ());
-            }
-            // Send query to database
-            if(!$this->_model->query($sql_insert)) {
-                die($this->_model->error());
-            }
-			performEvent('PostController/actionCreate',array($user,$content, time()+ZFramework::app()->timezone*60*60));
-            if(defined('API_MODE')){
-                $json_array=array('insert_id'=>  $this->_model->insert_id());
-                die (json_encode($json_array));
-            }
-            if(isset($_POST['ajax'])){
-                echo 'OK';
-                return TRUE;
-            }
+    public function actionCreate() {
+        is_post();
+        issetPostParam('user');
+        issetPostParam('content');
+        
+        //Validation
+        $new_data_error_msg='';
+        if ( !strlen(trim($_POST['user'])) || !strlen(trim($_POST['content']))) {
+            $new_data_error_msg=t('FILL_NOT_COMPLETE');
+        } elseif(strlen($_POST['content']>580)) {
+            $new_data_error_msg=t('WORDS_TOO_LONG');
+        } elseif (ZFramework::app()->valid_code_open==1 && gd_loaded() && !$this->_verifyCode->check($_POST['valid_code'])) {
+            $new_data_error_msg=t('CAPTCHA_WRONG');
         }
-        if(defined('API_MODE')){
-            $error_array=array('error_code'=>'400','error'=>$API_CODE['400'],'error_detail'=>t('ONLY_POST'));
-            die(json_encode($error_array));
+        if($new_data_error_msg){
+            exitWithResponse(400, array('response'=> $new_data_error_msg));
         }
-        header("Location:index.php");
+        
+        // Ready to send query to database
+        $user=  $this->_model->escape_string($_POST['user']);
+        if(!isset($_SESSION['admin']) && $_POST['user']==ZFramework::app()->admin ) {
+            $user='anonymous';
+        }
+        $userExists =  $this->_model->queryAll(sprintf(parse_tbprefix("SELECT * FROM <sysuser> WHERE username='%s'"),  $this->_model->escape_string($_POST['user'])));
+        if($userExists && (@$_SESSION['user']!=$_POST['user'])) {
+            $user='anonymous';
+        }
+        $content = $this->_model->escape_string($_POST['content']);
+        if(isset ($_SESSION['uid'])) {
+            $sql_insert= sprintf (parse_tbprefix("INSERT INTO <post> ( uid , content , post_time , ip ) VALUES ( %d , '%s' , %d , '%s' )"), $_SESSION['uid'],$content,  time (),  getIp ());
+        } else {
+            $sql_insert = sprintf (parse_tbprefix("INSERT INTO <post> ( uname , content , post_time , ip ) VALUES ( '%s' ,'%s' , %d , '%s')"), $user,$content,  time (),  getIp ());
+        }
+        // Send query to database
+        if(!$this->_model->query($sql_insert)) {
+            exitWithResponse(500, array('response'=>$this->_model->error()));
+        }
+        performEvent('PostController/actionCreate',array($user,$content, time()+ZFramework::app()->timezone*60*60));
+        $json_array=array('insert_id'=>  $this->_model->insert_id());
+        exitWithResponse(200, $json_array);
     }
+    
     public function actionUpdate(){
-        is_admin();
-	if(isset($_POST['Submit'])){
-	    $mid=(int)$_POST['mid'];
-	    $update_content = $this->_model->escape_string(str_replace(array("\n", "\r\n", "\r"), '', nl2br($_POST['update_content'])));
-            $this->_model->query(sprintf(parse_tbprefix("UPDATE <post> SET content='%s' WHERE pid=%d"),$update_content,$mid));
-            header("Location:index.php?action=control_panel&subtab=message");
-	}
+        isAdminAjaxRequest();
+        is_post();
+        issetPostParam('mid');
+        issetPostParam('update_content');
+        
+        $mid=(int)$_POST['mid'];        
+        $update_content = $this->_model->escape_string(str_replace(array("\n", "\r\n", "\r"), '', nl2br($_POST['update_content'])));
+        $this->_model->query(sprintf(parse_tbprefix("UPDATE <post> SET content='%s' WHERE pid=%d"),$update_content,$mid));
+        exitWithResponse(200, $json_array);
+    }
+    
+    public function actionShow() {
+        issetGETParam('mid');
+        
         $mid=(int)$_GET['mid'];
         $message_info=$this->_model->queryAll(sprintf(parse_tbprefix("SELECT * FROM <post> WHERE pid=%d"),$mid));
         if(!$message_info){
-            show_message(t('QUERY_ERROR'),TRUE,'index.php?action=control_panel&subtab=message');
+            exitWithResponse(500, array('response'=> t('QUERY_ERROR')));
         }
-	$message_info=$message_info[0];
-        $this->render('update', array(
-            'message_info'=>$message_info,
-            'mid'=>$mid,
-        ));
+        $message_info=$message_info[0];
+        exitWithResponse(200, array('response'=> $message_info));
     }
-    // TODO GET=> POST
-    public function actionDelete(){
-        is_admin();
-        $mid=isset ($_POST['mid'])?(int)$_POST['mid']:null;
-        if(!$mid){
-            header("Location:index.php?action=control_panel&amp;subtab=message");exit;
-        }
+    
+    public function actionDelete() {
+        isAdminAjaxRequest();
+        is_post();
+        issetPostParam('mid');
+        
+        $mid = (int)$_POST['mid'];
         $this->_model->query(parse_tbprefix("DELETE FROM <post> WHERE pid=$mid"));
         $this->_model->query(parse_tbprefix("DELETE FROM <reply> WHERE pid=$mid"));
-        if (defined('API_MODE')) {
-          header("Content-type: application/json");
-					$result=array('status'=>'OK');
-          die(json_encode($result));
-        }
-        header("Location:index.php?action=control_panel&subtab=message&randomvalue=".rand());
+        exitWithResponse(200);
     }
+    
     public  function actionDelete_multi_messages(){
-        is_admin();
-        if(!isset($_POST['select_mid'])){header("location:index.php?action=control_panel&subtab=message");exit;}
-	$del_ids=$_POST['select_mid'];
-        foreach($del_ids as $deleted_id){
+        isAdminAjaxRequest();
+        is_post();
+        issetPostParam('select_mid');
+        
+        $del_ids = $_POST['select_mid'];
+        foreach($del_ids as $deleted_id) {
             $this->_model->query(parse_tbprefix("DELETE FROM <post> WHERE pid=$deleted_id"));
             $this->_model->query(parse_tbprefix("DELETE FROM <reply> WHERE pid=$deleted_id"));
         }
-        header("Location:index.php?action=control_panel&subtab=message&randomvalue=".rand());
+        exitWithResponse(200);
     }
 
-    public  function actionDeleteAll(){
-        is_admin();
+    public  function actionDeleteAll() {
+        isAdminAjaxRequest();
+        is_post();
+        
         $this->_model->query(parse_tbprefix("DELETE FROM <post>"));
         $this->_model->query(parse_tbprefix("DELETE FROM <reply>"));
-        header("location:index.php?action=control_panel&subtab=message");
+        exitWithResponse(200);
     }
 
     public function actionList() {
