79ae4066b5b6e47dc1af92f830bb72303b25abd0
rainyjune <rainyjune@live.cn>
2015-12-13 13:31:03 +0800

    Update SearchController.php

--- includes/controllers/SearchController.php
+++ includes/controllers/SearchController.php
@@ -5,35 +5,14 @@ class SearchController extends BaseController{
         global $db_url;
         $this->_db=YDB::factory($db_url);
     }
+    
     public function actionIndex(){
-        global $API_CODE;
-        if(! empty($_REQUEST['s'])){
-            $s=trim($_REQUEST['s']);
-            if(isset ($_GET['ajax']) || defined('API_MODE')){ 
-              $data=get_all_data(false, true, false, true);
-            } else {
-              $data=get_all_data(true, true, false, true);
-            }
-            $result_array=array();
-            foreach($data as $_data){
-                if(strpos(@$_data['user'],$s)!==false || strpos($_data['post_content'], $s)!==false || strpos($_data['reply_content'], $s)!==false || strpos($_data['b_username'], $s)!==false) {
-                    $result_array[]=$_data;
-                }
-            }
-            if (defined('API_MODE')) {
-                $json_array=array('messages'=>$result_array,'nums'=>  count($result_array));
-                die (json_encode($json_array));
-            }
-            $nums=count($result_array);
-            $this->render('search_result',array(
-            'data'=>$result_array,
-            'nums'=>$nums,
-            ));
-        } elseif (defined('API_MODE')) {
-            $error_array=array('error_code'=>'400','error'=>$API_CODE['400'],'error_detail'=>t('NO_SEARCH_PARAM'));
-            die(json_encode($error_array));
-        }else{
-            header("Location:index.php");
-        }
+        issetRequestParam('s');
+        
+        $s = trim($_REQUEST['s']);
+        $sql=sprintf(parse_tbprefix("SELECT p.pid AS id, p.ip AS ip , p.uid AS uid ,p.uname AS uname,p.content AS post_content,p.post_time AS time,r.content AS reply_content,r.r_time AS reply_time ,u.username AS b_username FROM <post> AS p LEFT JOIN <reply> AS r ON p.pid=r.pid LEFT JOIN <sysuser> AS u ON p.uid=u.uid WHERE p.content LIKE '%%%s%%' OR r.content LIKE '%%%s%%' ORDER BY p.post_time DESC"), $s, $s);
+        $result_array=$this->_db->queryAll($sql);
+        $statusCode = count($result_array) ? 200 : 404;
+        exitWithResponse($statusCode,$result_array);
     }
 }
