4f26249487c0ce150f540a6e59e261c3b54fd768
rainyjune <rainyjune@live.cn>
2015-12-13 10:46:59 +0800

    Update BadipController.php

--- includes/controllers/BadipController.php
+++ includes/controllers/BadipController.php
@@ -14,32 +14,28 @@ class BadipController extends BaseController
     }
 
     public function actionCreate() {
-        is_admin();
-        $ip=@$_POST['ip'];
-        if (valid_ip($ip)==false) {
-            header("Location:index.php?action=control_panel&subtab=message");exit;
+        isAdminAjaxRequest();
+        is_post();
+        if (!isset($_POST['ip']) || (valid_ip($_POST['ip']) == false)) {
+            exitWithResponse(400);
         }
+        $ip = $_POST['ip'];
         if(is_baned($ip)) {
-            header("Location:index.php?action=control_panel&subtab=ban_ip");exit;
+            exitWithResponse(304);
         }
         $this->_model->query(sprintf(parse_tbprefix("INSERT INTO <badip> ( ip ) VALUES ( '%s' )"),$ip));
-        if (defined('API_MODE')) {
-          header("Content-type: application/json");
-          $result=array('status'=>'OK');
-          die(json_encode($result));
-        }
-        header("Location:index.php?action=control_panel&subtab=ban_ip");
+        exitWithResponse(200);
     }
 
     public function actionUpdate() {
-        is_admin();
-        @$ip_update_array=$_POST['select_ip'];
-        if(!$ip_update_array) {
-            header("Location:index.php?action=control_panel&subtab=ban_ip");exit;
+        isAdminAjaxRequest();
+        is_post();
+        if (!isset($_POST['select_ip'])) {
+            exitWithResponse(400);
         }
-        foreach ($ip_update_array as $_ip) {
+        foreach ($_POST['select_ip'] as $_ip) {
             $this->_model->query(sprintf(parse_tbprefix("DELETE FROM <badip> WHERE ip = '%s'"),$_ip));
         }
-        header("Location:index.php?action=control_panel&subtab=ban_ip");
+        exitWithResponse(200);
     }
 }
