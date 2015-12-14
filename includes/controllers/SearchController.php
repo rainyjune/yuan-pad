4d0494f845eb81eec07e56a4118e95160119ef54
rainyjune <rainyjune@live.cn>
2015-12-14 11:49:01 +0800

    Update SearchController.php

--- includes/controllers/SearchController.php
+++ includes/controllers/SearchController.php
@@ -8,11 +8,14 @@ class SearchController extends BaseController{
     
     public function actionIndex(){
         issetRequestParam('s');
-        
+        $result = array();
         $s = trim($_REQUEST['s']);
         $sql=sprintf(parse_tbprefix("SELECT p.pid AS id, p.ip AS ip , p.uid AS uid ,p.uname AS uname,p.content AS post_content,p.post_time AS time,r.content AS reply_content,r.r_time AS reply_time ,u.username AS b_username FROM <post> AS p LEFT JOIN <reply> AS r ON p.pid=r.pid LEFT JOIN <sysuser> AS u ON p.uid=u.uid WHERE p.content LIKE '%%%s%%' OR r.content LIKE '%%%s%%' ORDER BY p.post_time DESC"), $s, $s);
         $result_array=$this->_db->queryAll($sql);
-        $statusCode = count($result_array) ? 200 : 404;
-        exitWithResponse($statusCode,$result_array);
+        $numRows = count($result_array);
+        $statusCode = $numRows ? 200 : 404;
+        $result['comments'] = $result_array;
+        $result['total'] = $numRows;
+        exitWithResponse($statusCode, $result);
     }
 }
