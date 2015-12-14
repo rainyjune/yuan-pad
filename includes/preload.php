9ffc52bb58b291bfdb126f3cad64f53f350ca59d
rainyjune <rainyjune@live.cn>
2015-12-14 09:07:25 +0800

    Cleanup

--- includes/preload.php
+++ includes/preload.php
@@ -1,14 +1,19 @@
 <?php
 /**
  * Bootstrap file
- *
  * @author rainyjune <rainyjune@live.cn>
- * @version $Id$
+ * 
  */
 if(!defined('IN_MP')){die('Access denied!');}
-if(version_compare(PHP_VERSION,'5.1.0','<')){die('PHP Version 5.1.0+ required!');}
+if(version_compare(PHP_VERSION,'5.2.0','<')){die('PHP Version 5.2.0+ required!');}
 date_default_timezone_set('UTC');
-error_reporting(E_ALL);
+
+if (defined('DEBUG_MODE') && DEBUG_MODE) {
+    error_reporting(E_ALL);
+} else {
+    error_reporting(0);
+}
+
 require 'functions.php';
 
 ini_set('arg_separator.output',     '&amp;');
@@ -27,16 +32,16 @@ maple_unset_globals();
 //Load database library
 require APPROOT.'/includes/database/YDB.php';
 //Load the configuration file
-if(file_exists(conf_path().'/config.php'))
+if(file_exists(conf_path().'/config.php')) {
     include_once conf_path().'/config.php';
-else
+} else {
     include './sites/default/default.config.php';
+}
 
 define('CONFIGFILE', conf_path().'/config.php');
-define('MP_VERSION','1.0');
+define('MP_VERSION','1.2');
 define('THEMEDIR', 'themes/');
 define('PLUGINDIR', 'plugins/');
-define('SMILEYDIR', 'misc/images/');
 
 include_once 'Imgcode.php';
 // Load ZFramework
@@ -49,8 +54,7 @@ if(is_installed()){
     if(is_baned(getIP())) {
         die('Access denied!');
     }
-	is_closedMode();
-}
-elseif($_GET['action']!='install'){
+    is_closedMode();
+} elseif ($_GET['action']!='install'){
 	header("Location:index.php?action=install");exit;
 }
