7eabb4a8916773a92fd02a5cf93246b72466e790
rainyjune <rainyjune@live.cn>
2015-12-16 10:11:43 +0800

    Updated acp-overview.js

--- themes/spa/src/acp-overview.js
+++ themes/spa/src/acp-overview.js
@@ -1,6 +1,27 @@
 var React = require('react');
+var dataProvider = require('./dataProvider.js');
 
 var ACPOverview = React.createClass({
+  getInitialState: function() {
+    return {
+      commentsTotal: 0,
+      repliesTotal: 0,
+      appVersion: '',
+      phpVersion: '',
+      gdVersion: '',
+      registerGlobals: '',
+      magicQuotesGPC: '',
+      zipSupport: ''
+    };
+  },
+  componentDidMount: function() {
+    dataProvider.getAppOverviewInfo(function(res){
+      if (res.statusCode !== 200) {
+        return ;
+      }
+      this.setState(res.response);
+    }.bind(this));
+  },
   render: function() {
     var cssClass = this.props.activeTab === "overview" ? "selectTag" : "";
     return (
@@ -21,31 +42,31 @@ var ACPOverview = React.createClass({
               <td colSpan="2" ><b>{this.props.lang.STATS_INFO}</b></td>
             </tr>
             <tr>
-              <td >{this.props.lang.NUM_POSTS}：</td><td align="right">{this.props.acpData.nums}</td>
+              <td >{this.props.lang.NUM_POSTS}：</td><td align="right">{this.state.commentsTotal}</td>
             </tr>
             <tr>
-              <td >{this.props.lang.NUM_REPLY}：</td><td align="right">{this.props.acpData.reply_num}</td>
+              <td >{this.props.lang.NUM_REPLY}：</td><td align="right">{this.state.repliesTotal}</td>
             </tr>
             <tr>
-              <td >{this.props.lang.MP_VERSION}：</td><td align="right">{this.props.acpData.yuanpad_version}</td>
+              <td >{this.props.lang.MP_VERSION}：</td><td align="right">{this.state.appVersion}</td>
             </tr>
             <tr>
               <td  colSpan="2"><b>{this.props.lang.SYS_INFO}</b></td>
             </tr>
             <tr>
-              <td >{this.props.lang.PHP_VERSION}：</td><td align="right">{this.props.acpData.php_version}</td>
+              <td >{this.props.lang.PHP_VERSION}：</td><td align="right">{this.state.phpVersion}</td>
             </tr>
             <tr>
-              <td >{this.props.lang.GD_VERSION}： </td><td align="right">{this.props.acpData.gd_version}</td>
+              <td >{this.props.lang.GD_VERSION}： </td><td align="right">{this.state.gdVersion}</td>
             </tr>
             <tr>
-              <td >Register_Globals：</td><td align="right">{this.props.acpData.register_globals}</td>
+              <td >Register_Globals：</td><td align="right">{this.state.registerGlobals}</td>
             </tr>
             <tr>
-              <td >Magic_Quotes_Gpc：</td><td align="right">{this.props.acpData.magic_quotes_gpc}</td>
+              <td >Magic_Quotes_Gpc：</td><td align="right">{this.state.magicQuotesGPC}</td>
             </tr>
             <tr>
-              <td >ZipArchive：</td><td align="right">{this.props.acpData.zip_support}</td>
+              <td >ZipArchive：</td><td align="right">{this.state.zipSupport}</td>
             </tr>
           </tbody>
         </table>
