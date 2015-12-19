292d72629c6bc4f91734335e6c8544abf4174124
rainyjune <rainyjune@live.cn>
2015-12-19 11:49:20 +0800

    Update ReplyController.php

--- includes/controllers/ReplyController.php
+++ includes/controllers/ReplyController.php
@@ -18,11 +18,23 @@ class ReplyController extends BaseController{
         if (trim($reply_content)=='') {
             exitWithResponse(400, array('response'=>t('REPLY_EMPTY')));
         }
-        if(isset($_POST['update'])) {
-            $this->_model->query(sprintf(parse_tbprefix("UPDATE <reply> SET content='%s' WHERE pid=%d"),$reply_content,$mid));
-        } else {
-            $this->_model->query(sprintf(parse_tbprefix("INSERT INTO <reply> ( pid , content , r_time ) VALUES ( %d , '%s' , %d )"),$mid,$reply_content,time()));
+        $this->_model->query(sprintf(parse_tbprefix("INSERT INTO <reply> ( pid , content , r_time ) VALUES ( %d , '%s' , %d )"),$mid,$reply_content,time()));
+        exitWithResponse(200);
+    }
+    
+    public function actionUpdate() {
+        isAdminAjaxRequest();
+        is_post();
+        issetPostParam('mid');
+        issetPostParam('content');
+
+        $mid=(int)$_POST['mid'];
+        $reply_content = $this->_model->escape_string(str_replace(array("\n", "\r\n", "\r"), '', nl2br($_POST['content'])));
+        
+        if (trim($reply_content)=='') {
+            exitWithResponse(400, array('response'=>t('REPLY_EMPTY')));
         }
+        $this->_model->query(sprintf(parse_tbprefix("UPDATE <reply> SET content='%s' WHERE pid=%d"),$reply_content,$mid));
         exitWithResponse(200);
     }
     
