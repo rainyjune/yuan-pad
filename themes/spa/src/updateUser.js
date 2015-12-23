f12391ab782ce2b61e76424d93522366a8b2f773
rainyjune <rainyjune@live.cn>
2015-12-23 11:18:55 +0800

    Update updateUser.js

--- themes/spa/src/updateUser.js
+++ themes/spa/src/updateUser.js
@@ -16,64 +16,16 @@ const customStyles = {
 let UserUpdate = React.createClass({
   getInitialState() {
     return {
-      userUpdateErrorMsg: '',
-      userUpdateModalIsOpen: false
+      errorMsg: '',
+      modalIsOpen: false
     };
   },
-  openUserUpdateModal() {
-    this.setState({userUpdateModalIsOpen: true});
+  openModal() {
+    this.setState({modalIsOpen: true});
   },
-  closeUserUpdateModal() {
-    this.setState({userUpdateModalIsOpen: false});
+  closeModal() {
+    this.setState({modalIsOpen: false});
   },
-  handleUserUpdate(userData) {
-    dataProvider.updateUser(userData,  data => {
-        console.log('update user result:', data, userData);
-        if (data.error) {
-          if (this.isMounted()) {
-            this.setState({userUpdateErrorMsg: data.error_detail});
-          }
-        } else {
-          if (this.isMounted()) {
-            this.setState({userUpdateErrorMsg: '', userUpdateModalIsOpen: false});
-            this.props.onUserUpdated();
-          }
-        }
-      },
-      function(xhr, status, err) {
-        debugger;
-      }.bind(this));
-  },
-  render() {
-    return this.props.user.user_type === "regular" ?
-            (
-              <div className="updateUser">
-                <UserUpdateButton 
-                  lang={this.props.lang}
-                  onShowUpdateModal={this.openUserUpdateModal}
-                />
-                <UserUpdateModal
-                  lang={this.props.lang}
-                  user={this.props.user}
-                  userUpdateModalIsOpen={this.state.userUpdateModalIsOpen}
-                  userUpdateErrorMsg={this.state.userUpdateErrorMsg}
-                  onRequestClose={this.closeUserUpdateModal}
-                  onUserUpdateSubmit={this.handleUserUpdate}
-                />
-              </div>
-            ) : null;
-  }
-});
-
-let UserUpdateButton = React.createClass({
-  render() {
-    return (
-      <a href="javascript:void(0);" onClick={this.props.onShowUpdateModal}>{this.props.lang.UPDATE}</a>
-    );
-  }
-});
-
-let UserUpdateModal = React.createClass({
   handleSubmit(e) {
     e.preventDefault();
     let uid = this.refs.uid.value.trim(),
@@ -81,37 +33,54 @@ let UserUpdateModal = React.createClass({
         pwd = this.refs.pwd.value.trim(),
         email = this.refs.email.value.trim();
     if (!uid || !user || !email) return;
-    this.props.onUserUpdateSubmit({ uid, user, pwd, email}); 
+    dataProvider.updateUser({ uid, user, pwd, email},  res => {
+      debugger;
+      if (this.isMounted()) {
+        if (res.statusCode === 200) {
+          this.setState({errorMsg: '', modalIsOpen: false});
+          this.props.onCurrentUserUpdated(res.response);
+        } else {
+          alert(res.response);
+          this.setState({errorMsg: res.response});
+        }
+      }
+    });
     return false;
   },
-  render(){
+  render() {
+    let language = this.props.lang,
+        state = this.state,
+        user = this.props.user;
+        
     return (
-      <Modal isOpen={this.props.userUpdateModalIsOpen} onRequestClose={this.props.onRequestClose} style={customStyles} >
-        <h2>Update profile</h2>
-        <p>{this.props.userUpdateErrorMsg}</p>
-        <button onClick={this.props.onRequestClose}>close</button>
-        <form onSubmit={this.handleSubmit} action="#" method="post">
-        <input type="hidden" ref="uid" value={this.props.user.uid} />        
-          <dl>
-          <dt>{this.props.lang.USERNAME}</dt>
-          <dd><input type="text" readOnly="readonly" defaultValue={this.props.user.username} ref="user" size="20"  />
-          </dd>
-          </dl>
-          <dl>
-          <dt>{this.props.lang.PASSWORD}</dt>
-          <dd><input type="password" defaultValue={this.props.user.password} ref="pwd" size="20"  />
-          </dd>
-          </dl>
-          <dl>
-          <dt>{this.props.lang.EMAIL}</dt>
-          <dd><input type="text" defaultValue={this.props.user.email} ref="email" size="20"  />
-          </dd>
-          </dl>
-          <dl>
-              <dt><input type="submit" value={this.props.lang.UPDATE} /></dt>
-          </dl>
-        </form>
-      </Modal>
+      <div className="updateUser">
+        <a href="javascript:void(0);" onClick={this.openModal}>{language.UPDATE}</a>
+        <Modal isOpen={state.modalIsOpen} onRequestClose={this.closeModal} style={customStyles}>
+          <p>{state.errorMsg}</p>
+          <button onClick={this.closeModal}>close</button>
+          <form onSubmit={this.handleSubmit} action="#" method="post">
+            <input type="hidden" ref="uid" value={user.uid} />        
+            <dl>
+            <dt>{language.USERNAME}</dt>
+            <dd><input type="text" readOnly="readonly" defaultValue={user.username} ref="user" size="20"  />
+            </dd>
+            </dl>
+            <dl>
+            <dt>{language.PASSWORD}</dt>
+            <dd><input type="password" defaultValue={user.password} ref="pwd" size="20"  />
+            </dd>
+            </dl>
+            <dl>
+            <dt>{language.EMAIL}</dt>
+            <dd><input type="text" defaultValue={user.email} ref="email" size="20"  />
+            </dd>
+            </dl>
+            <dl>
+              <dt><input type="submit" value={language.UPDATE} /></dt>
+            </dl>
+          </form>
+        </Modal>
+      </div>
     );
   }
 });
