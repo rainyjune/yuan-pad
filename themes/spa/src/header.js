5dd3e8cba61eb7bcc6882b3100d52ddc59c0bc67
rainyjune <rainyjune@live.cn>
2015-12-23 10:07:07 +0800

    Cleanup: use ES6 spread attributes and arrows

--- themes/spa/src/header.js
+++ themes/spa/src/header.js
@@ -7,28 +7,23 @@ let SignIn = require('./signIn.js'),
 
 let Header = React.createClass({
   render() {
+    var props = {
+      user: this.props.user,
+      lang: this.props.lang,
+      onCurrentUserUpdated: this.props.onCurrentUserUpdated
+    };
     return (
       <div className="header">
-        <SignIn 
-          user={this.props.user} 
-          lang={this.props.lang} 
-          onUserSignedIn={this.props.onUserSignedIn}
-        />
-        <SignUp 
-          user={this.props.user} 
-          lang={this.props.lang} 
-          onSignedUp={this.props.onSignedUp}
-        />
-        <UpdateUser 
-          user={this.props.user}
-          lang={this.props.lang} 
-          onUserUpdated={this.props.onUserUpdated}
-        />
-        <SignOutButton 
-          user={this.props.user} 
-          lang={this.props.lang} 
-          onUserLogout={this.props.onUserLogout} 
-        />
+        {(()=> {
+          switch (this.props.user.user_type) {
+            case "guest":
+              return <div><SignIn {...props} /> <SignUp {...props} /></div>;
+            case "regular":
+              return <div><UpdateUser {...props} /><SignOutButton {...props} /></div>;
+            case "admin":
+              return <SignOutButton {...props} />;
+          }
+        })()}
       </div>
     );
   }
