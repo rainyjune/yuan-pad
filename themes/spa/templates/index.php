210ca07080da52df83f9f98de696a688576c670a
rainyjune <rainyjune@live.cn>
2015-12-27 15:07:46 +0800

    Include bootstrap CSS file in template files

--- themes/spa/templates/index.php
+++ themes/spa/templates/index.php
@@ -7,9 +7,9 @@
     <script src="./themes/spa/build/yuan.min.js"></script>
     <link rel="stylesheet" href="./themes/spa/css/style.css">
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="./themes/spa/css/bootstrap.min.css">
     <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <link rel="stylesheet" href="./themes/spa/css/bootstrap-theme.min.css">
   </head>
   <body>
     <div id="content"></div>
