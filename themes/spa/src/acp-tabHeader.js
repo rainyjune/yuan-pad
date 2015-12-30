3147a6389810c23d8c12e6a11162278dccd7aff0
rainyjune <rainyjune@live.cn>
2015-12-30 09:30:18 +0800

    Update acp-tabHeader.js

--- themes/spa/src/acp-tabHeader.js
+++ themes/spa/src/acp-tabHeader.js
@@ -57,29 +57,19 @@ let ACPTabHeader = React.createClass({
                 <span className="icon-bar"></span>
                 <span className="icon-bar"></span>
               </button>
+              {/*
               <a className="navbar-brand" href="#">Brand</a>
-              <a className="btn btn-default" href="index.php">{this.props.lang.HOME}</a>&nbsp;
-              <a className="btn btn-default" href="#" onClick={this.handleSignOut}>{this.props.lang.LOGOUT}</a>
+              */}
+              <a className="btn btn-default navbar-btn homeButton" href="index.php">{this.props.lang.HOME}</a>&nbsp;
+              <a className="btn btn-default navbar-btn signOutButton" href="#" onClick={this.handleSignOut}>{this.props.lang.LOGOUT}</a>
             </div>
         
             {/* Collect the nav links, forms, and other content for toggling */}
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <ul className="nav navbar-nav">
-                <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
-                <li><a href="#">Link</a></li>
-                <li className="dropdown">
-                  <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
-                  <ul className="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li role="separator" className="divider"></li>
-                    <li><a href="#">Separated link</a></li>
-                    <li role="separator" className="divider"></li>
-                    <li><a href="#">One more separated link</a></li>
-                  </ul>
-                </li>
+              <ul className="nav nav-pills nav-justified">
+                {items}
               </ul>
+              {/*
               <ul className="nav navbar-nav navbar-right">
                 <li><a href="#">Link</a></li>
                 <li className="dropdown">
@@ -93,12 +83,11 @@ let ACPTabHeader = React.createClass({
                   </ul>
                 </li>
               </ul>
+              */}
             </div>{/* /.navbar-collapse */}
           </div>{/* /.container-fluid */}
         </nav>
-        <ul className="nav nav-pills nav-justified">
-          {items}
-        </ul>
+        
       </div>
     );
   }
