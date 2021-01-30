5f02416c8b4835252e8287b431d75525409a7e66
rainyjune <rainyjune@live.cn>
2021-01-30 18:29:21 +0800

    Update CSS

--- themes/joys/src/acp-tabHeader.js
+++ themes/joys/src/acp-tabHeader.js
@@ -32,18 +32,29 @@ class ACPTabHeader extends React.Component {
     this.setState({menuIsOpen: !this.state.menuIsOpen});
   }
   render() {
+    const style1 = {
+      borderBottom: 0
+    };
+    const style2 = {
+      position: 'absolute',
+      bottom: 0,
+      left: '170px'
+    };
+    const style3 = {
+      borderRadius: '4px 4px 0 0'
+    };
     if (this.props.user.user_type !== "admin") return null;
     let activeTab = this.props.activeTab;
     let items = this.props.tabs.map(tab => {
       return (
         <li key={tab.value} role="presentation" className={tab.value === activeTab ? "active" : ""}>
-          <a href="#" data-tabname={tab.value} onClick={this.updateActiveTab}>{tab.text}</a>
+          <a href="#" style={style3} data-tabname={tab.value} onClick={this.updateActiveTab}>{tab.text}</a>
         </li>
       );
     });
     return (
       <div className="">
-        <nav className="navbar navbar-default">
+        <nav className="navbar navbar-default" style={style1}>
           <div className="container-fluid">
             <div className="navbar-header">
               <button onClick={this.toggleMenu} type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -56,14 +67,14 @@ class ACPTabHeader extends React.Component {
               <a className="btn btn-default navbar-btn signOutButton" href="#" onClick={this.handleSignOut}>{this.props.lang.LOGOUT}</a>
             </div>
             {/* Collect the nav links, forms, and other content for toggling */}
-            <div className={this.state.menuIsOpen ? "navbar-collapse collapse in" : "collapse navbar-collapse"} id="bs-example-navbar-collapse-1">
+            <div className={this.state.menuIsOpen ? "navbar-collapse collapse in" : "collapse navbar-collapse"} id="bs-example-navbar-collapse-1" style={style2}>
               <ul className="nav nav-pills nav-justified">
                 {items}
               </ul>
             </div>{/* /.navbar-collapse */}
           </div>{/* /.container-fluid */}
         </nav>
-        
+
       </div>
     );
   }
