2186a37baec4599f475d538b6ea7c24aae5f5f86
rainyjune <rainyjune@live.cn>
2022-12-12 10:27:04 +0800

    Update JS template files

--- themes/spa/templates/index.php
+++ themes/spa/templates/index.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
     <title><?php echo t('WELCOME',array('{site_name}'=>ZFramework::app()->board_name));?></title>
-    <script src="./themes/spa/build/yuan.min.js"></script>
     <link rel="stylesheet" href="./themes/spa/css/style.css">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="./themes/spa/css/bootstrap.min.css">
