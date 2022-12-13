ac40c499adf7cf77e96825273ca7934939c35628
rainyjune <rainyjune@live.cn>
2022-12-13 16:00:53 +0800

    Functional component

--- themes/spa/src/footer.js
+++ themes/spa/src/footer.js
@@ -1,29 +1,27 @@
 import React from 'react';
 
-class AppFooter extends React.Component {
-  ACPMarkup() {
-    let ACP = (this.props.user && this.props.user.user_type === "admin") ? 
-              "<a href='index.php?action=control_panel'>"+ this.props.lang.ACP+"</a>" 
+function AppFooter(props) {
+  const ACPMarkup = () => {
+    let ACP = (props.user && props.user.user_type === "admin") ? 
+              "<a href='index.php?action=control_panel'>"+ props.lang.ACP+"</a>" 
               : '';
     return {
       __html: ACP
     };
-  }
-  render() {
-    return (
-      <footer>
-        <p>
-          {this.props.appConfig.copyright_info}&nbsp;
-          <a href={"mailto:" + this.props.appConfig.admin_email}>{this.props.lang.ADMIN_EMAIL}</a>&nbsp;
-          <span dangerouslySetInnerHTML={this.ACPMarkup()}></span>
-        </p>
-        <p>
-          Powered by <a href="https://github.com/rainyjune/yuan-pad" target="_blank" title="Find More">YuanPad</a>&nbsp;
-          <a href="index.php?action=rss"><img src="misc/images/rss-icon.png" alt="rss" /></a>
-        </p>
-      </footer>
-    );
-  }
+  };
+  return (
+    <footer>
+      <p>
+        {props.appConfig.copyright_info}&nbsp;
+        <a href={"mailto:" + props.appConfig.admin_email}>{props.lang.ADMIN_EMAIL}</a>&nbsp;
+        <span dangerouslySetInnerHTML={ACPMarkup()}></span>
+      </p>
+      <p>
+        Powered by <a href="https://github.com/rainyjune/yuan-pad" target="_blank" title="Find More">YuanPad</a>&nbsp;
+        <a href="index.php?action=rss"><img src="misc/images/rss-icon.png" alt="rss" /></a>
+      </p>
+    </footer>
+  );
 }
 
 export default AppFooter;
