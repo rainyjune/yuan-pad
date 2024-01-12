160decc20c0c1ecbfd2ab22e10fe016df53205f2
rainyjune <rainyjune@live.cn>
2024-01-12 14:15:10 +0800

    replace function expressions with regular functions

--- themes/spa/src/acp/IPItem.tsx
+++ themes/spa/src/acp/IPItem.tsx
@@ -1,15 +1,11 @@
 export default function IPItem(props: any) {
-  const toggleItem = () => {
+  function toggleItem() {
     props.onItemToggled(props.data);
-  };
+  }
   return (
     <tr className="admin_message">
       <td>
-        <input
-          type="checkbox"
-          onChange={toggleItem}
-          checked={props.data.checked}
-        />
+        <input type="checkbox" onChange={toggleItem} checked={props.data.checked} />
       </td>
       <td>{props.data.ip}</td>
     </tr>
