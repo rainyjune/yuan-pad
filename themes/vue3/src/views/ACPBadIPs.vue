4128be7d47a1f0f7cc0ecc1ab18841a181a841ca
rainyjune <rainyjune@live.cn>
2024-06-08 13:38:43 +0800

    Use Element Plus

---
+++ themes/vue3/src/views/ACPBadIPs.vue
@@ -0,0 +1,3 @@
+<template>
+    Bad IPs
+</template>
--- themes/vue3/src/views/AdminPanelView.vue
+++ themes/vue3/src/views/AdminPanelView.vue
@@ -1,16 +1,33 @@
 <template>
-    <div class="about">
-      <h1>This is admin control panel page</h1>
-    </div>
-  </template>
-  
-  <style>
-  @media (min-width: 1024px) {
-    .about {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
-    }
-  }
-  </style>
-  
+  <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+    <el-tab-pane label="Default" name="default"><ACPDefault /></el-tab-pane>
+    <el-tab-pane label="User" name="users" lazy><ACPUsers /></el-tab-pane>
+    <el-tab-pane label="Config" name="config" lazy><ACPConfig /></el-tab-pane>
+    <el-tab-pane label="Messages" name="messages" lazy><ACPMessages /></el-tab-pane>
+    <el-tab-pane label="Bad IPs" name="badips" lazy><ACPBadIPs /></el-tab-pane>
+  </el-tabs>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import type { TabsPaneContext } from 'element-plus'
+import ACPDefault from './ACPDefault.vue';
+import ACPUsers from './ACPUsers.vue';
+import ACPConfig from './ACPConfig.vue';
+import ACPMessages from './ACPMessages.vue';
+import ACPBadIPs from './ACPBadIPs.vue';
+
+const activeName = ref('default')
+
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  console.log(tab, event)
+}
+</script>
+
+<style>
+.demo-tabs > .el-tabs__content {
+  padding: 32px;
+  color: #6b778c;
+  font-size: 32px;
+  font-weight: 600;
+}
+</style>
