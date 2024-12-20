f2b17eebef6fcb172ccb12f69ce940ac086ca810
rainyjune <rainyjune@live.cn>
2024-12-20 20:54:23 +0800

    Added avatar for users

--- themes/spa/src/common/utils.ts
+++ themes/spa/src/common/utils.ts
@@ -22,3 +22,22 @@ export function isValidItemsPerPage(itemsPerPage: any) {
       return false;
   }
 }
+
+export async function generateSHA256(message: string) {
+  // Encode the message as a Uint8Array
+  const encoder = new TextEncoder();
+  const data = encoder.encode(message);
+
+  // Use the SubtleCrypto API to compute the hash
+  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+
+  // Convert the hash buffer to a hex string
+  return Array.from(new Uint8Array(hashBuffer))
+    .map(byte => byte.toString(16).padStart(2, '0'))
+    .join('');
+}
+
+export async function generateGravatar(email = '', size = 100) {
+  const emailHash = await generateSHA256(email.trim().toLowerCase());
+  return `https://www.gravatar.com/avatar/${emailHash}?s=${size}&d=identicon`;
+}
--- themes/spa/src/front/Comment.tsx
+++ themes/spa/src/front/Comment.tsx
@@ -1,7 +1,20 @@
 import Reply from './Reply';
 import type { CommentProps } from '../common/types';
 
+import { generateGravatar } from '../common/utils';
+
+import PlaceHolderAvatar from '../images/example_avatar.jpg';
+import { useEffect, useState } from 'react';
+
 export default function Comment(props: CommentProps) {
+  const [avatar, setAvatar] = useState<string>(PlaceHolderAvatar);
+  useEffect(() => {
+    if (props?.data?.uname) {
+      generateGravatar(props.data.uname, 24).then((url) => {
+        setAvatar(url);
+      });
+    }
+  }, [props?.data?.uname]);
   function rawMarkup() {
     return { __html: props.children.toString() };
   }
@@ -10,10 +23,14 @@ export default function Comment(props: CommentProps) {
       __html: (Number(props.data.uid) ? props.data.b_username : props.data.uname) ?? '',
     };
   }
+
   return (
     <div className="comment py-3 flex flex-col gap-y-3">
       <div className="flex justify-between">
-        <span className="commentAuthor" dangerouslySetInnerHTML={rawAuthorMarkup()}></span>
+        <div className="flex gap-[10px]">
+          <img src={avatar} alt="avatar" className="w-[24px] h-[24px] rounded-full object-cover" />
+          <span className="commentAuthor" dangerouslySetInnerHTML={rawAuthorMarkup()}></span>
+        </div>
         <span className="commentDate">{props.data.time}</span>
       </div>
       <div className="commentText">
