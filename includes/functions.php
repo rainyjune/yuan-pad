3e7f4d4918fdaff255742b35b0be53781425bc66
rainyjune <rainyjune@live.cn>
2024-12-21 18:47:00 +0800

    Added polyfill for getallheaders function

--- includes/functions.php
+++ includes/functions.php
@@ -4,6 +4,18 @@
  * @version $Id$
  */
 
+ if (!function_exists('getallheaders')) {
+    function getallheaders() {
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
+
 /**
  * Validate IP Address
  *
